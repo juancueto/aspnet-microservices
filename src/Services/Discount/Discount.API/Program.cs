756697ad790e450d0d8678103bd002f521d4cd1d
Juan Cueto <jcueto@saexploration.com>
2022-11-28 12:00:41 -0500

    discount microservice

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,34 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MigrateDatabase<Program>();
+
+app.Run();
