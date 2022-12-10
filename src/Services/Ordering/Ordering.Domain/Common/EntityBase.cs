582662e3aa4473b89cf6d1d6346e2ff975043a55
Juan Cueto <jcueto@saexploration.com>
2022-12-10 18:54:27 -0500

    adding domain

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string Createdby { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
